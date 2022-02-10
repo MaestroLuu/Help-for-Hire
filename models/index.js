98315744a49b4cd38e09c16d515a0dc4f203b724
Marco Flores <marcoflores1029@gmail.com>
2022-02-10 12:46:26 -0800

    created relations between models in index-models, and exported the models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 const User = require("./User");
+const Job = require("./Job");
+
+Job.belongsTo(User, {
+    foreignKey: "user_id"
+});
 
 // Define sequelize associations in this file.
-
-module.exports = { User };
+module.exports = { User, Job };
