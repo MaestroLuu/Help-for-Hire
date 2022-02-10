beb0ccecf58fa1856513218701e587279938e194
chrismuto <chrismuto@gmail.com>
2022-02-10 11:34:29 -0800

    main.handlebar nav bar and formatting

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,21 @@ document.querySelector(".btn-logout").addEventListener("click", async () => {
     console.error("Failed to logout.");
   }
 });
+document.querySelector(".btn-logout").addEventListener("click", async () => {
+  try {
+    await fetch("/api/users/login", { method: "POST" });
+    document.location.replace("/login");
+  } catch (error) {
+    console.error(error);
+    console.error("Failed to logout.");
+  }
+});
+document.querySelector(".btn-logout").addEventListener("click", async () => {
+  try {
+    await fetch("/api/users/create-account", { method: "POST" });
+    document.location.replace("/create-account");
+  } catch (error) {
+    console.error(error);
+    console.error("Failed to logout.");
+  }
+});
