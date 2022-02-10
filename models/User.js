8f86dd158d113588f69feedbf61cf81e4e2b9d8c
Marco Flores <marcoflores1029@gmail.com>
2022-02-10 10:48:17 -0800

    removed username

--- models/User.js
+++ models/User.js
@@ -18,10 +18,6 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
