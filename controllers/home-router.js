4bf610986247aa5786da1a6820f12fe9790a51c1
Marco Flores <marcoflores1029@gmail.com>
2022-02-11 12:02:02 -0800

    fixed the routes in homeroutes so it can load handlebars

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -24,18 +24,18 @@ router.get("/home", async (req,res) => {
 
 // ADD WITHAUTH LATER ON
 router.get("/jobseeking", async (req, res) =>{
-  // // find all jobs in db
-  // const jobPosts = await Job.findAll({
-  //     // attributes: {exclude: req.session.userId},
-  // }).catch((err) => {
-  //     res.status(500).json(err);
-  // });
-  // //serialize jobs so that appropriate values can be displayed
-  // const jobs = jobPosts.map((posts) => posts.get({ plain:true}));
+  // find all jobs in db
+  const jobPosts = await Job.findAll({
+      // attributes: {exclude: req.session.userId},
+  }).catch((err) => {
+      res.status(500).json(err);
+  });
+  //serialize jobs so that appropriate values can be displayed
+  const jobs = jobPosts.map((posts) => posts.get({ plain:true}));
   
   // create a jobs.handlebars and partial cards for each job post
   res.render('job-seeking', {
-    // jobs,
+    jobs,
     title: "Job Seeking",
     // logged_in: req.session.logged_in,
   });
@@ -44,38 +44,38 @@ router.get("/jobseeking", async (req, res) =>{
 // directs to job description page
 // ADD WITHAUTH LATER ON
 router.get("/jobseeking/:id", async (req, res) =>{
-  // try {
-  //     const jobPost = await Job.findByPk(req.params.id);
-  //     if(!jobPost) {
-  //         res.status(404).json({ message: 'No job post exists with this id!'});
-  //         return;
-  //     }        
-  //     //serializes that specific job post 
-  //     const jobDescription = jobPost.get({ plain: true});
-  //     // create a description.handlebars
+  try {
+      const jobPost = await Job.findByPk(req.params.id);
+      if(!jobPost) {
+          res.status(404).json({ message: 'No job post exists with this id!'});
+          return;
+      }        
+      //serializes that specific job post 
+      const jobDescription = jobPost.get({ plain: true});
+      // create a description.handlebars
       res.render('job-details', {
-        // jobDescription,
+        jobDescription,
         title: "Job Description",
         // logged_in: req.session.logged_in,
       });
-  // } catch (err) {
-  //     res.status(500).json(err)
-  // }
+  } catch (err) {
+      res.status(500).json(err)
+  }
 });
 
 //missing cooking information
 // ADD WITHAUTH LATER ON
 router.get("/hiringposts", async (req, res) =>{
-  // const jobPosts = await Job.findAll({where: {id: req.body.id}}).catch((err) => {
-  //     res.status(500).json(err);
-  // });
+  const jobPosts = await Job.findAll({}).catch((err) => {
+      res.status(500).json(err);
+  });
   
-  // //serialize jobs so that appropriate values can be displayed
-  // const jobs = jobPosts.map((posts) => posts.get({ plain:true}));
+  //serialize jobs so that appropriate values can be displayed
+  const jobs = jobPosts.map((posts) => posts.get({ plain:true}));
 
   // create hiring handlebars
   res.render ('hiring-posts', {
-      // jobs, 
+      jobs, 
       // where: {id: req.body.id},
       title: "Hiring Posts",
       // logged_in: req.session.logged_in
